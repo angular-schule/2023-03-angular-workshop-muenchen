82a69d468a9b42d0fc1b2b691d06779ae271db3b
Ferdinand Malcher <ferdinand@malcher.media>
2023-03-13 17:17:21 +0100

    Aufgaben: RatingComponent, disable, Anzahl

--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { BooksRoutingModule } from './books-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
+import { RatingComponent } from './rating/rating.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    BookComponent
+    BookComponent,
+    RatingComponent
   ],
   imports: [
     CommonModule,
