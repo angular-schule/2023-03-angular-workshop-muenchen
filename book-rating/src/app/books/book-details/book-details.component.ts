a9b9489606e4190b12d52a70bdf816c679150c79
Ferdinand Malcher <ferdinand@malcher.media>
2023-03-14 16:55:30 +0100

    Routing

---
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
+
+@Component({
+  selector: 'br-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.scss']
+})
+export class BookDetailsComponent {
+  book?: Book;
+
+  constructor(private route: ActivatedRoute, private bookStoreService: BookStoreService) {
+    // PULL
+    // const isbn = this.route.snapshot.paramMap.get('isbn');
+
+    // PUSH
+    this.route.paramMap.subscribe(params => {
+      const isbn = params.get('isbn')!; // Non-Null Assertion (böse!)
+      this.bookStoreService.getSingle(isbn).subscribe(book => {
+        this.book = book;
+      });
+    });
+
+    // Aufgabe:
+    // - Buch abrufen (HTTP)
+    // - anzeigen (ganz simpel!)
+  }
+}
--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -5,13 +5,15 @@ import { BooksRoutingModule } from './books-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
 import { RatingComponent } from './rating/rating.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
     BookComponent,
-    RatingComponent
+    RatingComponent,
+    BookDetailsComponent
   ],
   imports: [
     CommonModule,
