e48a1fc7ac0dcee863c5c08d02f37b1eb270e793
Ferdinand Malcher <ferdinand@malcher.media>
2023-03-15 14:50:23 +0100

    Formular abschicken

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-book-create',
@@ -7,6 +10,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./book-create.component.scss']
 })
 export class BookCreateComponent {
+
   bookForm = new FormGroup({
     isbn: new FormControl('', {
       nonNullable: true,
@@ -30,6 +34,26 @@ export class BookCreateComponent {
       ]
     }),
   });
+
+  constructor(private bookStoreService: BookStoreService, private router: Router, private route: ActivatedRoute) {}
+
+  submitForm() {
+    const newBook: Book = {
+      ...this.bookForm.getRawValue(),
+      authors: [],
+      description: '',
+      price: 1
+    };
+
+    this.bookStoreService.create(newBook).subscribe(receivedBook => {
+      this.router.navigate(['/books', receivedBook.isbn]); // Detailseite
+      // this.router.navigateByUrl('/books/' + receivedBook.isbn) // Detailseite
+      // this.router.navigateByUrl('/books') // Dashboard
+      // this.router.navigate(['..'], { relativeTo: this.route });
+    });
+
+  }
+
 }
 
 /*
